a5ddc04a440bce6386e4921f88e2da35ad8a3a46
mrtx87 <patrickbdoerfer@gmail.com>
2025-02-13 16:56:43 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+
+yarn run build
+cp 404.html dist
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:mrtx87/german-election-2025-coalitions.git master:gh-pages
